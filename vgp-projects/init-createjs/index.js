abd0ebe040a35d0a90d77375fe252b4d0c208611
EGMartinezz <emely.martinez@sciacademy.org>
2025-09-17 00:36:45 +0000

    completed init createjs

--- vgp-projects/init-createjs/index.js
+++ vgp-projects/init-createjs/index.js
@@ -1,50 +1,86 @@
-/*
- * TODO 4: Create a modularized index.js, 
- * pass in window and createjs
- */
 (function(window, createjs) {
-  // TODO 5: Initialize CreateJS //
-  
+  const canvas = document.getElementById("canvas");
+  const stage = new createjs.Stage(canvas);
 
-  // TODO 6: Set the framerate of the Ticker
-  
+  createjs.Ticker.framerate = 60;
 
+  const bg = new createjs.Shape();
+  bg.graphics
+    .beginFill("rgba(100, 6, 6, 1)")
+    .drawCircle(250, 250, 250); 
 
-  /*
-   * TODO 7:CREATE AND CONFIGURE ANY DISPLAY 
-   * OBJECTS AND ADD THEM TO THE DISPLAY LIST HERE
-   */
-  
-  // INIT CREATEJS //
+  const eyeContainer = new createjs.Container(); 
+  eyeContainer.x = 200;
+  eyeContainer.y = 200;
 
-  
+  const leftEye = new createjs.Shape(); 
+  const rightEye = new createjs.Shape();
 
-    
-  // CREATE A BACKGROUND //
+  leftEye.graphics
+    .beginFill("rgba(250, 162, 162, 1)")
+    .drawRoundRect(-80, -10, 80, 20, 5); 
 
-    
-  // CREATE A CIRCLE //
+  rightEye.graphics
+    .beginFill("rgba(250, 162, 162, 1)")
+    .drawRoundRect(40, -10, 80, 20, 5); 
 
-  
+  eyeContainer.addChild(leftEye, rightEye);
+  stage.addChild(bg, eyeContainer);
 
+  stage.update();
 
-  // ADD DISPLAY OBJECTS TO STAGE //
+  let tickHandler = createjs.Ticker.on("tick", onTick); 
+  let blinkTimer = 0;
+  let areEyesOpen = true;
+  let leftEyeHeight = 20;
+  let rightEyeHeight = 20;
 
+  function onTick(event){
+    update(event);
+  }
 
+  function update(event){
+    blinkTimer++;
 
-  // TODO 8: Listen to the 'tick' event  //
-  
-  
+    if (areEyesOpen && blinkTimer > 120) {
+      areEyesOpen = false;
+      blinkTimer = 0;
+    } else if (!areEyesOpen && blinkTimer > 15) {
+      areEyesOpen = true;
+      blinkTimer = 0;
+    }
 
-  // TODO 9: Handle the 'tick' event //
-  
-  
+    if (areEyesOpen) {
+      leftEyeHeight += 1;
+      if (leftEyeHeight > 20) {
+        leftEyeHeight = 20;
+      }
+      rightEyeHeight += 1;
+      if (rightEyeHeight > 20) {
+        rightEyeHeight = 20;
+      }
+    } else {
+      leftEyeHeight -= 1;
+      if (leftEyeHeight < 0) {
+        leftEyeHeight = 0;
+      }
+      rightEyeHeight -= 1;
+      if (rightEyeHeight < 0) {
+        rightEyeHeight = 0;
+      }
+    }
 
-  /*
-   * TODO 10: Implement an update Function, after making 
-   * changes to assets, it must call stage.update(); 
-   */
-  
-  
+    leftEye.graphics.clear();
+    leftEye.graphics
+      .beginFill("rgba(250, 162, 162, 1)")
+      .drawRoundRect(-80, -10 + (20 - leftEyeHeight) / 2, 80, leftEyeHeight, 5); 
+
+    rightEye.graphics.clear();
+    rightEye.graphics
+      .beginFill("rgba(250, 162, 162, 1)")
+      .drawRoundRect(40, -10 + (20 - rightEyeHeight) / 2, 80, rightEyeHeight, 5); 
+
+    stage.update();
+  }
 
 }(window, window.createjs));
